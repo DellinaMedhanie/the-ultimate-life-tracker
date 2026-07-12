b863aa509fd3653b168d051ce0004235a77616ce
Mary Felsheim-Lee <87479435+sonnywuzhere@users.noreply.github.com>
2026-07-11 23:31:51 -0400

    add DashboardController class

---
+++ src/main/DashboardController.java
@@ -0,0 +1,18 @@
+package main;
+
+// Controller Layer
+public class DashboardController {
+	
+	public void initialize() {
+		
+	} 
+	
+	public static void onAddTransaction() {
+		TransactionForm form = new TransactionForm();
+		form.main(null);
+	} 
+	
+	public void onViewCharts() {
+		
+	}
+}
